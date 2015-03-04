ebe8d0cb3e7c383f3c9955e75fc0cc8c49fc944b
Brian Anderson <banderson@dandb.com>
2015-03-03 20:15:58 -0800

    initial commit

---
+++ database/migrations/2015_03_03_180234_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /* This is the first migration. Before running the migration, we run this script as root:
+        CREATE DATABASE usps;
+        CREATE USER 'usps'@'localhost' IDENTIFIED BY 'usps1234';
+        GRANT ALL PRIVILEGES ON usps . * TO 'usps'@'localhost';
+        FLUSH PRIVILEGES;
+        */
+        Schema::create('users', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('password_resets', function(Blueprint $table)
+        {
+            $table->string('email')->index();
+            $table->string('token')->index();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+
+        Schema::drop('password_resets');
+    }
+
+}
